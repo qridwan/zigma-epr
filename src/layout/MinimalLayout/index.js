c1afc9d096f7952e3a30563a50d4ee77bbb78afc
Mohammad Ridwanul Alam <myworld.shoyab@gmail.com>
2022-09-27 12:29:45 +0600

    auth ui done

--- src/layout/MinimalLayout/index.js
+++ src/layout/MinimalLayout/index.js
@@ -1,15 +1,29 @@
+import { Grid, Stack, useMediaQuery, useTheme } from '@mui/material';
 import { Outlet } from 'react-router-dom';
-
-// project imports
-import Customization from '../Customization';
+import authbg from '../../assets/images/auth/auth-bg.svg';
 
 // ==============================|| MINIMAL LAYOUT ||============================== //
 
-const MinimalLayout = () => (
-    <>
-        <Outlet />
-        {/* <Customization /> */}
-    </>
-);
+const MinimalLayout = () => {
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down('md'));
+    return (
+        <>
+            <Grid container sx={{ height: '100%', backgroundColor: theme.palette.primary.light, minHeight: '100vh' }}>
+                {!isMobile && (
+                    <Grid item md={6}>
+                        <Stack direction="row" alignItems="center" justifyContent="center" sx={{}} mt={20}>
+                            <img src={authbg} alt="auth" width="80%" />
+                        </Stack>
+                    </Grid>
+                )}
+                <Grid item md={4}>
+                    <Outlet />
+                </Grid>
+            </Grid>
+            {/* <Customization /> */}
+        </>
+    );
+};
 
 export default MinimalLayout;
